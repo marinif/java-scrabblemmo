51ca369d0c225c4134e541391ac74dba04708ba3
Fabio Marini <fabio.marini996@mekomidev.com>
2017-03-09 12:20:27 +0100

    Colori caselle parzialmente completo

--- src/game/Griglia.java
+++ src/game/Griglia.java
@@ -1,13 +1,26 @@
 package game;
 
 public class Griglia {
-	private char[][] caselle;
+	private Casella[][] caselle;
 	
 	public Griglia(){
 		for(int i =0;i<15;i++){
 			for(int j =0;j<15;j++){
-				caselle[i][j]=' ';
+				caselle[i][j]=new Casella();
+				if((i==0 || i==7 || i==14) && (j==0 || j==7 || j==14) && (i != 7 && j != 7)){
+					caselle[i][j].setCasella("ROSSO");
+				}
+				else if(i == j)
+					caselle[i][j].setCasella("GIALLO");
+				else
+					caselle[i][j].setCasella("VERDE");
+				
 			}
 		}
+		caselle[0][3].setCasella("BIANCO");
+		caselle[0][11].setCasella("BIANCO");
+		caselle[1][5].setCasella("BLU");
+		caselle[1][9].setCasella("BLU");
+		caselle[1][13].setCasella("GIALLO");
 	}
 }
