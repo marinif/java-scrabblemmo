ccc6c256f82ed0698d6e613469e69f110001b6ce
Fabio Marini <fabiozaru996@gmail.com>
2017-04-09 16:33:48 +0200

    funzione calcola punteggio e classe parola

---
+++ src/Parola.java
@@ -0,0 +1,29 @@
+
+public class Parola {
+	private int xi;
+	private int yi;
+	private int xf;
+	private int yf;
+	private int punteggio;
+	
+	public Parola(int xi,int yi,int xf,int yf,int punteggio){
+		this.xi=xi;
+		this.yi=yi;
+		this.xf=xf;
+		this.yf=yf;
+		this.punteggio=punteggio;
+	}
+	public int getXi(){
+		return xi;
+	}
+	
+	public int getXf(){
+		return xf;
+	}
+	public int getYi(){
+		return yi;
+	}
+	public int getYf(){
+		return yf;
+	}
+}
