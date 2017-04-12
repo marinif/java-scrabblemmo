209edf5f5fea7b662b0d6856270a8ad851f54a19
Fabio Marini <fabiozaru996@gmail.com>
2017-04-12 18:48:59 +0200

    condizioni fine partita #3

--- src/game/server/GameServer.java
+++ src/game/server/GameServer.java
@@ -43,20 +43,21 @@ public class GameServer extends Thread {
 		try {
 			Giocatore current = playerOne;
 			int contTurni=0;
+			/*
+			//conto quanti turni vengono fatti senza mettere parole sulla plancia
+			if(passa o cambia)
+				contTurni++;
+			else contTurni=0;
+			//condizioni per la fine della partita
+			if(bag.isEmpty() == true || contTurni > 2) {
+				// Invia messaggio fine a entrambi i giocatori
+				
+				// Chiudi le connessioni
+			}
+			*/
 			while(running) {
 				loop(current);
-				/*
-				//conto quanti turni vengono fatti senza mettere parole sulla plancia
-				if(passa o cambia)
-					contTurni++;
-				else contTurni=0;
-				//condizioni per la fine della partita
-				if(bag.isEmpty() == true || contTurni > 2) {
-					// Invia messaggio fine a entrambi i giocatori
-					
-					// Chiudi le connessioni
-				}
-				*/
+				
 				
 				// Swap players
 				current = (current == playerOne ? playerTwo : playerOne);
